f0de115b45c394b638c7035acc0cc65bec69272c
ledeni56 <lovro.ludvig@gmail.com>
2023-05-15 10:18:02 +0200

    Adjust theme

--- app/src/main/java/com/undabot/newsnow/ui/theme/Color.kt
+++ app/src/main/java/com/undabot/newsnow/ui/theme/Color.kt
@@ -2,10 +2,5 @@ package com.undabot.newsnow.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val primary = Color(0xFFCFDCF6)
+val primaryLight = Color(0xFFF3F7FF)
