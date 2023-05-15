56110c9b7be28a943be34ddb70fd1d596da39b60
ledeni56 <lovro.ludvig@gmail.com>
2023-05-15 12:02:02 +0200

    Adjust article card design

--- app/src/main/java/com/undabot/newsnow/ui/home/HomeScreenContent.kt
+++ app/src/main/java/com/undabot/newsnow/ui/home/HomeScreenContent.kt
@@ -4,10 +4,10 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
@@ -19,9 +19,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.undabot.newsnow.domain.model.Article
 import com.undabot.newsnow.domain.model.Source
+import com.undabot.newsnow.ui.components.NetworkImage
 
 @Composable
 fun HomeScreenContent(
@@ -36,7 +38,10 @@ fun HomeScreenContent(
       LazyRow(
         modifier = Modifier
           .statusBarsPadding()
-          .padding(top = 30.dp),
+          .padding(
+            top = 30.dp,
+            bottom = 8.dp,
+          ),
       ) {
         items(state.availableSources) { source ->
           SourceButton(
@@ -48,13 +53,16 @@ fun HomeScreenContent(
       }
     }
     items(state.articles) { article ->
-      ArticleCard(article)
+      ArticleCard(
+        article = article,
+        onClick = { onArticleClick(article) },
+      )
     }
   }
 }
 
 @Composable
-private fun ArticleCard(article: Article) {
+private fun ArticleCard(article: Article, onClick: () -> Unit) {
   Box(
     modifier = Modifier
       .padding(
@@ -66,14 +74,33 @@ private fun ArticleCard(article: Article) {
         elevation = 3.dp,
         shape = RoundedCornerShape(8.dp),
       )
-      .background(MaterialTheme.colorScheme.surface),
+      .background(MaterialTheme.colorScheme.surface)
+      .clickable(onClick = onClick),
   ) {
-    Column(
-      modifier = Modifier.padding(16.dp),
+    Row(
+      modifier = Modifier.padding(16.dp)
     ) {
-      Text(text = article.title)
-      Spacer(modifier = Modifier.height(8.dp))
-      Text(text = article.description)
+      NetworkImage(
+        modifier = Modifier.size(96.dp),
+        url = article.urlToImage,
+      )
+      Column(
+        modifier = Modifier.padding(start = 16.dp)
+      ) {
+        Text(
+          text = article.title,
+          style = MaterialTheme.typography.titleSmall,
+          maxLines = 2,
+          overflow = TextOverflow.Ellipsis,
+        )
+        Text(
+          modifier = Modifier.padding(2.dp),
+          text = article.description,
+          style = MaterialTheme.typography.bodySmall,
+          maxLines = 3,
+          overflow = TextOverflow.Ellipsis,
+        )
+      }
     }
   }
 }
