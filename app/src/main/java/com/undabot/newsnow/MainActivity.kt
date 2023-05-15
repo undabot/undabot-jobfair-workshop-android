452cdd9575dce29fcc43a66457161510c46119a4
ledeni56 <lovro.ludvig@gmail.com>
2023-05-15 10:26:02 +0200

    Make app go behind status bar

--- app/src/main/java/com/undabot/newsnow/MainActivity.kt
+++ app/src/main/java/com/undabot/newsnow/MainActivity.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
 import com.undabot.newsnow.ui.home.HomeScreen
 import com.undabot.newsnow.ui.theme.NewsNowTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -16,15 +18,24 @@ class MainActivity : ComponentActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+
+    drawAppBehindSystemBars()
+
     setContent {
       NewsNowTheme {
         Surface(
-          modifier = Modifier.fillMaxSize(),
+          modifier = Modifier
+            .fillMaxSize()
+            .navigationBarsPadding(),
           color = MaterialTheme.colorScheme.background,
         ) {
           HomeScreen()
         }
       }
     }
+  }
+
+  private fun drawAppBehindSystemBars() {
+    WindowCompat.setDecorFitsSystemWindows(window, false)
   }
 }
--- app/src/main/java/com/undabot/newsnow/ui/theme/Theme.kt
+++ app/src/main/java/com/undabot/newsnow/ui/theme/Theme.kt
@@ -1,8 +1,15 @@
 package com.undabot.newsnow.ui.theme
 
+import android.app.Activity
+import android.os.Build
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val LightColorScheme = lightColorScheme(
   primary = primary,
@@ -23,6 +30,25 @@ private val LightColorScheme = lightColorScheme(
 fun NewsNowTheme(
   content: @Composable () -> Unit,
 ) {
+
+  val view = LocalView.current
+
+  SideEffect {
+    val window = (view.context as Activity).window
+
+    window.statusBarColor = Color.Transparent.toArgb()
+    window.navigationBarColor = Color.Transparent.toArgb()
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+      window.isNavigationBarContrastEnforced = false
+    }
+
+    val windowsInsetsController = WindowCompat.getInsetsController(window, view)
+
+    windowsInsetsController.isAppearanceLightStatusBars = true
+    windowsInsetsController.isAppearanceLightNavigationBars = true
+  }
+
   MaterialTheme(
     colorScheme = LightColorScheme,
     typography = Typography,
